748711e489b4d11c9579ea57334c9b40f4ba5dd8
Veena Rajesh Badgujar <veenabadgujar23629@gmail.com>
2022-02-23 23:13:22 +0530

    Assignments

---
+++ Phase1/Assisted Project/Collection.java
@@ -0,0 +1,44 @@
+package Phase1;
+
+import java.util.*;
+
+public class Collection{
+
+	public static void main(String[] args) {
+		//creating arraylist
+		System.out.println("ArrayList");
+		ArrayList<String> city=new ArrayList<String>();   
+	      city.add("Jalgaon");//
+	      city.add("UK");    	   
+	      System.out.println(city);  
+		
+		//creating vector
+	      System.out.println("\n");
+	      System.out.println("Vector");
+	      Vector<Integer> vec = new Vector();
+	      vec.addElement(15); 
+	      vec.addElement(30); 
+	      System.out.println(vec);
+		
+	       //creating hashset
+	       System.out.println("\n");
+	       System.out.println("HashSet");
+	       HashSet<Integer> set=new HashSet<Integer>();  
+	       set.add(101);  
+	       set.add(103);  
+	       set.add(102);
+	       set.add(104);
+	       System.out.println(set);
+	       
+	       //creating linkedhashset
+	       System.out.println("\n");
+	       System.out.println("LinkedHashSet");
+	       LinkedHashSet<Integer> set2=new LinkedHashSet<Integer>();  
+	       set2.add(11);  
+	       set2.add(13);  
+	       set2.add(12);
+	       set2.add(14);	       
+	       System.out.println(set2);
+	      	} 
+	      }  
+	
