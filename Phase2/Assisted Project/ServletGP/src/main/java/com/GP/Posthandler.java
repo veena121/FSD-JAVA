9dd6dc216aa5408e55ca6b213c6a96840714539e
Veena Rajesh Badgujar <veenabadgujar23629@gmail.com>
2022-03-02 12:07:55 +0530

    Assignments

---
+++ Phase2/Assisted Project/ServletGP/src/main/java/com/GP/Posthandler.java
@@ -0,0 +1,52 @@
+package com.GP;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Posthandler
+ */
+@WebServlet("/Posthandler")
+public class Posthandler extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Posthandler() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+        PrintWriter out = response.getWriter();
+        out.println("Welcome This Is My Website");
+        
+        String name = request.getParameter("Swati");
+        String address = request.getParameter("Pune");
+        
+        out.println("Name=" + name + "<br>Address=" + address);
+        out.println("Welcome This Is My Website");
+
+	}
+
+}
